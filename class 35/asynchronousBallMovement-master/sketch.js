b39aff438f14afc1963f5d76ef6b7c7175cdd43e
mrityunjaygiri <78855357+mrityunjaygiri@users.noreply.github.com>
2021-07-10 14:14:32 +0530

    Add files via upload

---
+++ class 35/asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,55 @@
+var ball;
+var database;
+var ballPosition, position;
+
+function setup(){
+
+    database = firebase.database();
+    createCanvas(500,500);
+
+    ball = createSprite(200,200,10,10);
+    ball.shapeColor = "red";
+
+    ballPosition = database.ref('ball/position');
+    ballPosition.on("value", readPosition, showError);
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+
+        'x' : position.x + x,
+        'y' : position.y + y
+    })
+
+  
+}
+
+function readPosition(data){
+   position = data.val();
+   ball.x = position.x;
+   ball.y = position.y;
+
+}
+
+function showError(){
+
+    console.log("There is an error while reading the value from database");
+}
